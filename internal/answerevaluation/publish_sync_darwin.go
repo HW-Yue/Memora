cd46300e9c88f5018519be7aba2821de3aa96fae
HW-Yue <161243157+HW-Yue@users.noreply.github.com>
2026-08-06 23:10:21 +0800

    fix: publish answer evaluation on ExFAT

---
+++ internal/answerevaluation/publish_sync_darwin.go
@@ -0,0 +1,25 @@
+//go:build darwin
+
+package answerevaluation
+
+import (
+	"errors"
+	"os"
+
+	"golang.org/x/sys/unix"
+)
+
+func syncDirectory(path string) error {
+	directory, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer directory.Close()
+	if err := directory.Sync(); err != nil {
+		if errors.Is(err, unix.EOPNOTSUPP) || errors.Is(err, unix.ENOTSUP) || errors.Is(err, unix.EINVAL) {
+			return nil
+		}
+		return err
+	}
+	return nil
+}
